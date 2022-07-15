e955e3f7421167e2353d22558fa79633da4a8b6b
조세정 <97869635+sj4238@users.noreply.github.com>
2022-07-15 23:14:06 +0900

    Vue 조건문 v-if와 모달창 만들기(Vue에서 동적인 UI 만드는 법)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
   
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
   </div>
+
   <div>
-    <h4>{{products[0]}}</h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{products[0]}}</h4>
     <p>50만원</p>
     <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
   </div>
   <div>
+    <img src="./assets/room1.jpg" class="room-img">
     <h4>{{products[1]}}</h4>
-    <p>50만원</p>
+    <p>60만원</p>
     <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" class="room-img">
     <h4>{{products[2]}}</h4>
-    <p>50만원</p>
+    <p>70만원</p>
     <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
   </div>
 </template>
@@ -26,6 +38,7 @@ export default {
   name: 'App',
   data(){
     return {
+      모달창열렸니 : false,
       신고수 : [0,0,0],     
       메뉴들 : ['Home','Shop','About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
@@ -43,6 +56,27 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background-color: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background-color: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
