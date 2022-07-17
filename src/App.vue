8a3e70b835f652ba18b80bf7c0081078b8685bc0
조세정 <97869635+sj4238@users.noreply.github.com>
2022-07-18 02:28:54 +0900

    실제데이터넣기(import / export )

--- src/App.vue
+++ src/App.vue
@@ -12,32 +12,23 @@
     <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
   </div>
 
-  <div>
-    <img src="./assets/room0.jpg" class="room-img">
-    <h4 @click="모달창열렸니 = true">{{products[0]}}</h4>
-    <p>50만원</p>
-    <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
+  <div v-for="(a, i) in 원룸들" :key="i">
+    <img :src="a.image" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{a.title}}</h4>
+    <p>{{a.price}}원</p>
   </div>
-  <div>
-    <img src="./assets/room1.jpg" class="room-img">
-    <h4>{{products[1]}}</h4>
-    <p>60만원</p>
-    <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
-  </div>
-  <div>
-    <img src="./assets/room2.jpg" class="room-img">
-    <h4>{{products[2]}}</h4>
-    <p>70만원</p>
-    <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
-  </div>
+  
 </template>
 
 <script>
+
+import data from './assets/oneroom.js';
 
 export default {
   name: 'App',
   data(){
     return {
+      원룸들 : data,
       모달창열렸니 : false,
       신고수 : [0,0,0],     
       메뉴들 : ['Home','Shop','About'],
